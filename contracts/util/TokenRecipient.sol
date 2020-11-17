a374f7156109bfa700ad5edc5421e3aabed1749f
Lumyo <lumyo.b@gmail.com>
2020-11-17 14:00:24 +0800

    Fixed IERC20 path in TokenRecipient.sol

--- contracts/util/TokenRecipient.sol
+++ contracts/util/TokenRecipient.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.6.0;
 
-import '@openzeppelin/contracts/token/IERC20/IIERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract TokenRecipient {
   event ReceivedEther(address indexed sender, uint256 amount);
