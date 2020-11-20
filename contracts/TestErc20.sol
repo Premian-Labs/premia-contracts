9a13b50303f03fc535dac84e0ec58030de3ff796
Lumyo <lumyo.b@gmail.com>
2020-11-20 16:14:08 +0800

    More PremiaOption tests

---
+++ contracts/TestErc20.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Only used for unit tests
+contract TestErc20 is ERC20 {
+    constructor() public ERC20("Test", "TEST") {
+    }
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+    }
+}
--- test/PremiaOption-test.ts
+++ test/PremiaOption-test.ts
@@ -1,37 +1,100 @@
 import { ethers } from 'hardhat';
-import { utils } from 'ethers';
+import { BigNumberish, utils } from 'ethers';
 import { expect } from 'chai';
-import { ERC20Factory, PremiaOptionFactory } from '../contractsTyped';
-import { ERC20 } from '../contractsTyped/ERC20';
+import { PremiaOptionFactory, TestErc20Factory } from '../contractsTyped';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { PremiaOption } from '../contractsTyped/PremiaOption';
+import { TestErc20 } from '../contractsTyped/TestErc20';
+
+let eth: TestErc20;
+let dai: TestErc20;
+let premiaOption: PremiaOption;
+let user1: SignerWithAddress;
+let user2: SignerWithAddress;
+
+interface WriteOptionArgs {
+  address?: string;
+  expiration?: number;
+  strikePrice?: BigNumberish;
+  isCall?: boolean;
+  contractAmount?: number;
+}
+
+async function addEth() {
+  return premiaOption.addToken(
+    eth.address,
+    utils.parseEther('1'),
+    utils.parseEther('10'),
+  );
+}
+
+async function writeOption(args?: WriteOptionArgs) {
+  return premiaOption.writeOption(
+    args?.address ?? eth.address,
+    args?.expiration ?? 1687996800,
+    args?.strikePrice ?? utils.parseEther('10'),
+    args?.isCall ?? true,
+    args?.contractAmount == undefined ? 1 : args?.contractAmount,
+  );
+}
 
 describe('PremiaOption', function () {
-  let eth: ERC20;
-  let dai: ERC20;
-  let premiaOption: PremiaOption;
-  let user1: SignerWithAddress;
-  let user2: SignerWithAddress;
-
   beforeEach(async () => {
     [user1, user2] = await ethers.getSigners();
-    const erc20Factory = new ERC20Factory(user1);
-    eth = await erc20Factory.deploy('ether', 'ETH');
-    dai = await erc20Factory.deploy('dai', 'DAI');
+    const erc20Factory = new TestErc20Factory(user1);
+    eth = await erc20Factory.deploy();
+    dai = await erc20Factory.deploy();
 
     const premiaOptionFactory = new PremiaOptionFactory(user1);
     premiaOption = await premiaOptionFactory.deploy('dummyURI', dai.address);
   });
 
   it('Should add eth for trading', async function () {
-    await premiaOption.addToken(
-      eth.address,
-      utils.parseEther('1'),
-      utils.parseEther('10'),
-    );
+    await addEth();
     const settings = await premiaOption.tokenSettings(eth.address);
     expect(settings.contractSize.eq(utils.parseEther('1'))).to.true;
     expect(settings.strikePriceIncrement.eq(utils.parseEther('10'))).to.true;
     expect(settings.isDisabled).to.false;
+  });
+
+  describe('writeOption', () => {
+    it('should fail if token not added', async () => {
+      await expect(writeOption()).to.be.revertedWith('Token not supported');
+    });
+
+    it('should revert if contract amount <= 0', async () => {
+      await addEth();
+      await expect(writeOption({ contractAmount: 0 })).to.be.revertedWith(
+        'Contract amount must be > 0',
+      );
+    });
+
+    it('should revert if contract strike price <= 0', async () => {
+      await addEth();
+      await expect(writeOption({ strikePrice: 0 })).to.be.revertedWith(
+        'Strike price must be > 0',
+      );
+    });
+
+    it('should revert if strike price increment is wrong', async () => {
+      await addEth();
+      await expect(
+        writeOption({ strikePrice: utils.parseEther('1') }),
+      ).to.be.revertedWith('Wrong strikePrice increment');
+    });
+
+    it('should revert if timestamp already passed', async () => {
+      await addEth();
+      await expect(writeOption({ expiration: 1605858784 })).to.be.revertedWith(
+        'Expiration already passed',
+      );
+    });
+
+    it('should revert if timestamp increment is wrong', async () => {
+      await addEth();
+      await expect(writeOption({ expiration: 1687996801 })).to.be.revertedWith(
+        'Wrong expiration timestamp increment',
+      );
+    });
   });
 });
