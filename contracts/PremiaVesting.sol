5ad1a39d7dce5be67ceda3f69b87aef64a5b8421
Lumyo <lumyo.b@gmail.com>
2021-01-05 02:45:50 +0800

    PremiaVesting.sol

---
+++ contracts/PremiaVesting.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract PremiaVesting is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public premia;
+
+    // The timestamp at which release ends
+    uint256 public endTimestamp;
+    // The length of the release period (Once this period is passed, amount is fully unlocked)
+    uint256 public releasePeriod = 365 days;
+    // The timestamp at which last withdrawal has been done
+    uint256 public lastWithdrawalTimestamp;
+
+    constructor() public {
+        uint256 timestamp = getBlockTimestamp();
+        endTimestamp = timestamp.add(releasePeriod);
+        lastWithdrawalTimestamp = timestamp;
+    }
+
+    function getBlockTimestamp() public view returns(uint256) {
+        return block.timestamp;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 timestamp = getBlockTimestamp();
+
+        if (timestamp == lastWithdrawalTimestamp) return;
+
+        uint256 _lastWithdrawalTimestamp = lastWithdrawalTimestamp;
+        lastWithdrawalTimestamp = timestamp;
+
+        uint256 balance = premia.balanceOf(address(this));
+
+        if (timestamp >= endTimestamp) {
+            premia.safeTransfer(msg.sender, balance);
+        } else {
+            uint256 elapsedSinceLastWithdrawal = timestamp.sub(_lastWithdrawalTimestamp);
+            uint256 timeLeft = endTimestamp.sub(_lastWithdrawalTimestamp);
+
+            premia.safeTransfer(msg.sender, balance.mul(timeLeft).div(elapsedSinceLastWithdrawal));
+        }
+    }
+}
