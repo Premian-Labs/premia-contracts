8cc9b0994ccba134dac460acae12d02cd9415bf4
Lumyo <lumyo.b@gmail.com>
2021-01-07 12:28:19 +0800

    PremiaFeeDiscount.sol : Added events + ability to migrate stake

--- contracts/PremiaFeeDiscount.sol
+++ contracts/PremiaFeeDiscount.sol
@@ -8,8 +8,11 @@ import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import '@openzeppelin/contracts/utils/SafeCast.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
 
-contract PremiaFeeDiscount is Ownable {
+import "./interface/INewPremiaFeeDiscount.sol";
+
+contract PremiaFeeDiscount is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using SafeCast for uint256;
@@ -37,12 +40,22 @@ contract PremiaFeeDiscount is Ownable {
     // List of all existing stake periods
     uint256[] public existingStakePeriods;
 
+    // In case we want to upgrade this contract
+    // Users will have to migrate their stake manually by calling migrateStake() so that there is no risk of funds being drained
+    INewPremiaFeeDiscount public newContract;
+
     // Staking levels
     StakeLevel[] public stakeLevels;
 
-    ///////////
+    ////////////
+    // Events //
+    ////////////
 
-    // ToDo : Add events
+    event Staked(address indexed user, uint256 amount, uint256 stakePeriod, uint256 lockedUntil);
+    event Unstaked(address indexed user, uint256 amount);
+    event StakeMigrated(address indexed user, address newContract, uint256 amount, uint256 stakePeriod, uint256 lockedUntil);
+
+    ///////////
 
     constructor(IERC20 _xPremia) public {
         xPremia = _xPremia;
@@ -51,6 +64,11 @@ contract PremiaFeeDiscount is Ownable {
     ///////////
     // Admin //
     ///////////
+
+    // Set a new PremiaFeeDiscount contract, to enable migration
+    function setNewContract(INewPremiaFeeDiscount _newContract) external onlyOwner {
+        newContract = _newContract;
+    }
 
     // Set a stake period multiplier
     function setStakePeriod(uint256 _secondsLocked, uint256 _multiplier) external onlyOwner {
@@ -78,10 +96,22 @@ contract PremiaFeeDiscount is Ownable {
 
     ///////////
 
+    // Allow a user to migrate their stake to a new PremiaFeeDiscount contract, while preserving same lockup expiration date
+    function migrateStake() external nonReentrant {
+        require(newContract != address(0), "Migration disabled");
+
+        UserInfo memory user = userInfo[msg.sender];
+
+        delete userInfo[msg.sender];
+
+        newContract.migrate(msg.sender, user.balance, user.stakePeriod, user.lockedUntil);
+        emit StakeMigrated(msg.sender, newContract, user.balance, user.stakePeriod, user.lockedUntil);
+    }
+
 
     // Stake specified amount. The amount will be locked for the given period.
     // Longer period of locking will apply a multiplier on the amount staked, in the fee discount calculation
-    function stake(uint256 _amount, uint256 _period) external {
+    function stake(uint256 _amount, uint256 _period) external nonReentrant {
         require(stakePeriods[_period] > 0, "Stake period does not exists");
         UserInfo storage user = userInfo[msg.sender];
 
@@ -91,10 +121,12 @@ contract PremiaFeeDiscount is Ownable {
         xPremia.transferFrom(msg.sender, address(this), _amount);
         user.balance = user.balance.add(_amount);
         user.lockedUntil = lockedUntil.toUint64();
+
+        emit Staked(msg.sender, _amount, _period, lockedUntil);
     }
 
     // Unstake specified amount
-    function unstake(uint256 _amount) external {
+    function unstake(uint256 _amount) external nonReentrant {
         UserInfo storage user = userInfo[msg.sender];
 
         // We allow unstake if the stakePeriod that the user used has been disabled
@@ -102,6 +134,8 @@ contract PremiaFeeDiscount is Ownable {
 
         user.balance = user.balance.sub(_amount);
         xPremia.transfer(msg.sender, _amount);
+
+        emit Unstaked(msg.sender, _amount);
     }
 
     // Return number of stake levels
