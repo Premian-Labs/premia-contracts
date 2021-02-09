f6d969c6190b322ab9b62b016e1b80bd31f3922b
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-08 19:14:56 -0600

    add Openhedge core contract

---
+++ contracts/Openhedge.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+pragma abicoder v2;
+
+import '@solidstate/contracts/contracts/access/OwnableStorage.sol';
+import '@solidstate/contracts/contracts/architecture/diamond/DiamondBase.sol';
+
+/**
+ * @title Openhedge core contract
+ * @dev based on the EIP2535 Diamond standard
+ */
+contract Openhedge is DiamondBase {
+  using DiamondBaseStorage for DiamondBaseStorage.Layout;
+
+  /**
+   * @notice deploy contract and connect given diamond facets
+   * @param cuts diamond cuts to add at deployment
+   * @param pairImplementation implementaion Pair contract
+   */
+  constructor (
+    DiamondBaseStorage.FacetCut[] memory cuts,
+    address pairImplementation
+  ) {
+    OwnableStorage.layout().owner = msg.sender;
+    DiamondBaseStorage.layout().diamondCut(cuts);
+  }
+}
