427ada3e6c0f771fed886b841a4c7f9336ea08dc
steven <steven.nevins.js@gmail.com>
2021-02-25 22:41:01 -0500

    added accounts task to troubleshoot

---
+++ tasks/accounts.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async () => {
+    const accounts = await ethers.getSigners();
+  
+    for (const account of accounts) {
+      console.log(account.address);
+    }
+  });
