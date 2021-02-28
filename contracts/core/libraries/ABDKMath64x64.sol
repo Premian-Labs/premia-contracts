362baee4339fd7e5cb223324decbb51370a90a19
steven <steven.nevins.js@gmail.com>
2021-02-28 12:45:40 -0500

    removed unchecked from pow

--- contracts/core/libraries/ABDKMath64x64.sol
+++ contracts/core/libraries/ABDKMath64x64.sol
@@ -342,10 +342,10 @@ library ABDKMath64x64 {
 
     if (negativeResult) {
       require (absoluteResult <= 0x80000000000000000000000000000000);
-      return unchecked {-int128(int256(absoluteResult));} // We rely on overflow behavior here
+      return -int128(int256(absoluteResult)); // We rely on overflow behavior here
     } else {
       require (absoluteResult <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
-      return unchecked {int128(int256(absoluteResult));} // We rely on overflow behavior here
+      return int128(int256(absoluteResult)); // We rely on overflow behavior here
     }
   }
 
