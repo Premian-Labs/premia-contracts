2d9042f5f090dd1509ae8ed8119fc52a3d5314d8
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-28 17:32:56 -0600

    add empty ABDKMath64x64 tests

---
+++ test/libraries/ABDKMath64x64.js
@@ -0,0 +1,201 @@
+describe('ABDKMath64x64', function () {
+  let instance;
+
+  before(async function () {
+    const factory = await ethers.getContractFactory('ABDKMath64x64Mock');
+    instance = await factory.deploy();
+    await instance.deployed();
+  });
+
+  describe('#fromInt', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#toInt', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#fromUInt', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#toUInt', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#from128x128', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#to128x128', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#add', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#sub', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#mul', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#muli', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#mulu', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#div', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#divi', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#divu', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#neg', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#abs', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#inv', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#avg', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#gavg', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#pow', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#sqrt', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#log_2', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#ln', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+
+  describe('#exp_2', function () {
+    it('todo');
+
+    describe('reverts if', function () {
+      it('todo');
+    });
+  });
+});
