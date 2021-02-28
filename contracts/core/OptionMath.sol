b9bce9468ae4fcd8b05dc3ba2a1bdb249fea2020
steven <steven.nevins.js@gmail.com>
2021-02-28 12:46:03 -0500

    Fixidity -> ABDK

--- contracts/core/OptionMath.sol
+++ contracts/core/OptionMath.sol
@@ -8,7 +8,7 @@ import { ABDKMath64x64 } from "./libraries/ABDKMath64x64.sol";
 contract OptionMath {
 
     function logreturns(uint256 close, uint256 close_1back) internal pure returns (uint256){
-        return uint256(ABDKMath64x64.ln(int256(close/close_1back)));
+        return uint256(ABDKMath64x64.to128x128(ABDKMath64x64.ln(ABDKMath64x64.fromUInt(close/close_1back))));
     }
 
     function rollingEma(uint256 _old, uint256 _current, uint256 _alpha) internal pure returns (uint256){
@@ -24,19 +24,19 @@ contract OptionMath {
     }
 
     function p(uint256 _var, uint256 _strike, uint256 _price, uint256 _days) internal pure returns (uint256) {
-        return (uint256(ABDKMath64x64.ln(int256(_strike/_price))) + _var/2 * _days)/ABDKMath64x64.sqrt(_var*_days);
+        return (ABDKMath64x64.toUInt(ABDKMath64x64.ln(ABDKMath64x64.fromUInt(_strike/_price))) + _var/2 * _days)/ ABDKMath64x64.toUInt(ABDKMath64x64.sqrt(ABDKMath64x64.fromUInt(_var*_days)));
     }
 
     function bsPrice(uint256 _var, uint256 _strike, uint256 _price, uint256 _timestamp) internal view returns (uint256) {
         require(_timestamp > block.timestamp, 'Option in the past');
         uint256 maturity = (_timestamp - block.timestamp) / (1 days);
         uint256 prob = p(_var, _strike, _price, maturity);
-        return _price * prob - _strike * uint256(ABDKMath64x64.exp(int256(maturity))) * prob;
+        return _price * prob - _strike * ABDKMath64x64.toUInt((ABDKMath64x64.exp(ABDKMath64x64.fromUInt(maturity)))) * prob;
     }
 
     function slippageFn(uint256 _Ct, uint256 _St, uint256 _St1) internal pure returns (uint256){
-        int256 exp = int256((_St1 - _St) / max(_St, _St1));
-        return _Ct * uint256(ABDKMath64x64.inv(ABDKMath64x64.exp(exp)));
+        uint256 exp = (_St1 - _St) / max(_St, _St1);
+        return _Ct * uint256(ABDKMath64x64.to128x128(ABDKMath64x64.inv(ABDKMath64x64.exp(ABDKMath64x64.fromUInt(exp)))));
     }
 
     function pT(uint256 _var, uint256 _strike, uint256 _price, uint256 _timestamp, uint256 _Ct, uint256 _St, uint256 _St1) internal view returns (uint256) {
