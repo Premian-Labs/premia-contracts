2c5ed6c648199d6b3c1cb112fde9622b6a9dd5f0
steven <steven.nevins.js@gmail.com>
2021-02-28 11:47:45 -0500

    removed fixidity math

--- contracts/core/OptionMath.sol
+++ contracts/core/OptionMath.sol
@@ -3,14 +3,12 @@
 pragma solidity ^0.8.0;
 
 import '@solidstate/contracts/utils/Math.sol';
-import { ExponentLib } from "./libraries/ExponentLib.sol";
-import { FixidityLib } from "./libraries/FixidityLib.sol";
-import { LogarithmLib } from "./libraries/LogarithmLib.sol";
+import { ABDKMath64x64 } from "./libraries/ABDKMath64x64.sol";
 
 contract OptionMath {
 
     function logreturns(uint256 close, uint256 close_1back) internal pure returns (uint256){
-        return uint256(LogarithmLib.log_b(10, int256(close/close_1back)));
+        return uint256(ABDKMath64x64.ln(int256(close/close_1back)));
     }
 
     function rollingEma(uint256 _old, uint256 _current, uint256 alpha) internal pure returns (uint256){
@@ -26,32 +24,23 @@ contract OptionMath {
     }
 
     function p(uint256 _var, uint256 _strike, uint256 _price, uint256 _days) internal pure returns (uint256) {
-        return (uint256(LogarithmLib.ln(int256(_strike/_price))) + _var/2 * _days)/sqrt(_var*_days);
+        return (uint256(ABDKMath64x64.ln(int256(_strike/_price))) + _var/2 * _days)/ABDKMath64x64.sqrt(_var*_days);
     }
 
     function bsPrice(uint256 _var, uint256 _strike, uint256 _price, uint256 _timestamp) internal view returns (uint256) {
         require(_timestamp > block.timestamp, 'Option in the past');
         uint256 maturity = (_timestamp - block.timestamp) / (1 days);
         uint256 prob = p(_var, _strike, _price, maturity);
-        return _price * prob - _strike * uint256(ExponentLib.powerE(int256(maturity))) * prob;
+        return _price * prob - _strike * uint256(ABDKMath64x64.exp(int256(maturity))) * prob;
     }
 
     function slippageFn(uint256 _Ct, uint256 _St, uint256 _St1) internal pure returns (uint256){
         int256 exp = int256((_St1 - _St) / max(_St, _St1));
-        return _Ct * uint256(FixidityLib.reciprocal(ExponentLib.powerE(exp)));
+        return _Ct * uint256(ABDKMath64x64.inv(ABDKMath64x64.exp(exp)));
     }
 
     function pT(uint256 _var, uint256 _strike, uint256 _price, uint256 _timestamp, uint256 _Ct, uint256 _St, uint256 _St1) internal view returns (uint256) {
         return slippageFn(_Ct, _St, _St1) * bsPrice(_var, _strike, _price, _timestamp);
-    }
-
-    function sqrt(uint256 x) internal pure returns (uint256 y) {
-        uint256 z = (x + 1) / 2;
-        y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
-        }
     }
 
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
