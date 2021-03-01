cd05f9a83537b0f89761f3d924b153f3e2505ad3
bapecorn-premia <bapecorn@protonmail.com>
2021-02-28 19:00:40 -0800

    Add todo to short/long pools

--- contracts/amm/PremiaLongUnderlyingPool.sol
+++ contracts/amm/PremiaLongUnderlyingPool.sol
@@ -8,13 +8,25 @@ import './PremiaLiquidityPool.sol';
 contract PremiaLongUnderlyingPool is PremiaLiquidityPool {
   function writeOptionFor(address receiver, address optionContract, uint256 optionId, uint256 amount, address premiumToken, uint256 amountPremium, address referrer) external {
     super.writeOptionFor(receiver, optionContract, optionId, amount, premiumToken, amountPremium, referrer);
+
+    // TODO: 
+    // 1. Take loan from short pools
+    // 2. Buy underlying token with loan token on dex to long it
   }
 
   function unwindOptionFor(address sender, address optionContract, uint256 optionId, uint256 amount, address premiumToken, uint256 amountPremium) external {
     super.unwindOptionFor(receiver, optionContract, optionId, amount, premiumToken, amountPremium);
+
+    // TODO: 
+    // 1. Sell underlying token bought earlier
+    // 2. Repay loan to short pool
   }
 
   function unlockCollateralFromOption(address optionContract, uint256 optionId, uint256 amount) external {
     super.unlockCollateralFromOption(optionContract, optionId, amount);
+
+    // TODO: 
+    // 1. Sell underlying token bought earlier
+    // 2. Repay loan to short pool
   }
 }
--- contracts/amm/PremiaShortUnderlyingPool.sol
+++ contracts/amm/PremiaShortUnderlyingPool.sol
@@ -8,13 +8,25 @@ import './PremiaLiquidityPool.sol';
 contract PremiaShortUnderlyingPool is PremiaLiquidityPool {
   function writeOptionFor(address receiver, address optionContract, uint256 optionId, uint256 amount, address premiumToken, uint256 amountPremium, address referrer) external {
     super.writeOptionFor(receiver, optionContract, optionId, amount, premiumToken, amountPremium, referrer);
+
+    // TODO: 
+    // 1. Take loan from call pools
+    // 2. Sell loaned token on dex to short it
   }
 
   function unwindOptionFor(address sender, address optionContract, uint256 optionId, uint256 amount, address premiumToken, uint256 amountPremium) external {
     super.unwindOptionFor(receiver, optionContract, optionId, amount, premiumToken, amountPremium);
+
+    // TODO: 
+    // 1. Sell loaned token bought earlier
+    // 2. Repay loan from call pool
   }
 
   function unlockCollateralFromOption(address optionContract, uint256 optionId, uint256 amount) external {
     super.unlockCollateralFromOption(optionContract, optionId, amount);
+
+    // TODO: 
+    // 1. Sell loaned token bought earlier
+    // 2. Repay loan from call pool
   }
 }
