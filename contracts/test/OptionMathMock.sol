8f720b0987e101e299a31b1e63bfadbe7bb1a4fe
Nick Barry <itsnickbarry@protonmail.ch>
2021-03-07 13:21:28 -0600

    add framework for OptionMath tests

--- contracts/test/OptionMathMock.sol
+++ contracts/test/OptionMathMock.sol
@@ -3,7 +3,100 @@
 import { OptionMath } from '../libraries/OptionMath.sol';
 
 contract OptionMathMock {
-  function test () external pure returns (uint) {
-    return 0;
+  function logreturns (
+    int256 today,
+    int256 yesterday
+  ) external view returns (int256) {
+    return OptionMath.logreturns(today, yesterday);
+  }
+
+  function rollingEma (
+    int256 _old,
+    int256 _current,
+    int256 _window
+  ) external view returns () {
+    return OptionMath.rollingEma(_old, _current, _window);
+  }
+
+  function rollingAvg (
+    int256 _old,
+    int256 _current,
+    int256 _window
+  ) external view returns (int256) {
+    return OptionMath.rollingAvg(_old, _current, _window);
+  }
+
+  function rollingVar (
+    int256 _yesterday,
+    int256 _today,
+    int256 _yesterdayaverage,
+    int256 _todayaverage,
+    int256 _yesterdayvariance,
+    int256 _window
+  ) external view returns (int256) {
+    return OptionMath.rollingVar(_yesterday, _today, _yesterdayaverage, _todayaverage, _yesterdayvariance, _window);
+  }
+
+  function p (
+    uint256 _variance,
+    uint256 _strike,
+    uint256 _price,
+    int128 _maturity
+  ) external view returns (uint256) {
+    return OptionMath.p(_variance, _strike, _price, _timestamp);
+  }
+
+  function bsPrice (
+    uint256 _variance,
+    uint256 _strike,
+    uint256 _price,
+    uint256 _timestamp
+  ) external view returns (uint256) {
+    return OptionMath.bsPrice(_variance, _strike, _price, _timestamp);
+  }
+
+  function cFn (
+    uint256 _Ct,
+    uint256 _St,
+    uint256 _St1
+  ) external view returns (uint256) {
+    return OptionMath.cFn(_Ct, _St, _St1);
+  }
+
+  function pT (
+    uint256 _price,
+    uint256 _variance,
+    uint256 _timestamp,
+    uint256 _Ct,
+    uint256 _St,
+    uint256 _St1
+  ) external view returns (uint256) {
+    return OptionMath.pT(_price, _variance, _timestamp, _Ct, _St, _St1);
+  }
+
+  function approx_pT (
+    uint256 _price,
+    uint256 _variance,
+    uint256 _timestamp,
+    uint256 _Ct,
+    uint256 _St,
+    uint256 _St1
+  ) external view returns (uint256) {
+    return OptionMath.approx_pT(_price, _variance, _timestamp, _Ct, _St, _St1);
+  }
+
+  function approx_Bsch (
+    int256 _price,
+    int256 _variance,
+    uint256 _timestamp
+  ) external view returns (int256) {
+    return OptionMath.approx_Bsch(_price, _variance, _timestamp);
+  }
+
+  function max (
+    uint256 a,
+    uint256 b
+  ) external view returns (uint256) {
+    return OptionMath.max(a, b);
   }
 }
---
+++ test/libraries/OptionMath.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+
+describe('ABDKMath64x64', function() {
+  let instance;
+
+  before(async function() {
+    const factory = await ethers.getContractFactory('OptionMathMock');
+    instance = await factory.deploy();
+    await instance.deployed();
+  });
+
+  describe('#logreturns', function () {
+    it('todo');
+  });
+
+  describe('#rollingEma', function () {
+    it('todo');
+  });
+
+  describe('#rollingAvg', function () {
+    it('todo');
+  });
+
+  describe('#rollingVar', function () {
+    it('todo');
+  });
+
+  describe('#p', function () {
+    it('todo');
+  });
+
+  describe('#bsPrice', function () {
+    it('todo');
+  });
+
+  describe('#cFn', function () {
+    it('todo');
+  });
+
+  describe('#pT', function () {
+    it('todo');
+  });
+
+  describe('#approx_pT', function () {
+    it('todo');
+  });
+
+  describe('#approx_Bsch', function () {
+    it('todo');
+  });
+
+  describe('#max', function () {
+    it('todo');
+  });
+});
