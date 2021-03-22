49b883ea012631f53584c08c13b1a1323eb622a8
steven <steven.nevins.js@gmail.com>
2021-03-21 21:01:21 -0400

    added revert tests

--- test/libraries/OptionMath.js
+++ test/libraries/OptionMath.js
@@ -1,20 +1,38 @@
 const { expect } = require('chai');
 
-describe('ABDKMath64x64', function() {
+const toFixed = function (bn) {
+  return bn.shl(64);
+};
+
+describe('OptionMath', function () {
   let instance;
 
-  before(async function() {
+  before(async function () {
     const factory = await ethers.getContractFactory('OptionMathMock');
     instance = await factory.deploy();
     await instance.deployed();
   });
 
   describe('#logreturns', function () {
-    it('todo');
+    it('returns the natural log returns for a given day', async function () {
+      const inputs = [1].map(
+        ethers.BigNumber.from
+      );
+      for (let bn of inputs) {
+        expect(instance.callStatic.logreturns(bn, bn)).not.to.be.reverted;
+      }
+    });
   });
 
   describe('#rollingEma', function () {
-    it('todo');
+    it('return the rolling ema value', async function () {
+      const inputs = [1].map(
+        ethers.BigNumber.from
+      );
+      for (let bn of inputs) {
+        expect(instance.callStatic.rollingEma(bn, bn, bn)).not.to.be.reverted;
+      }
+    });
   });
 
   describe('#rollingEmaVar', function () {
