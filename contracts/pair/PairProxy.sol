93bc63ed15204a8e464328e28ce43a5b39103210
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-08 00:17:28 -0500

    initialize Pair period and window

--- contracts/pair/PairProxy.sol
+++ contracts/pair/PairProxy.sol
@@ -20,8 +20,15 @@ contract PairProxy is ManagedProxyOwnable {
     address asset1
   ) ManagedProxy(IProxyManager.getPairImplementation.selector) {
     OwnableStorage.layout().owner = msg.sender;
+
     PoolProxy pool0 = new PoolProxy(msg.sender, asset0, asset1);
     PoolProxy pool1 = new PoolProxy(msg.sender, asset1, asset0);
-    PairStorage.layout().setPools(address(pool0), address(pool1));
+
+    {
+      PairStorage.Layout storage l = PairStorage.layout();
+      l.setPools(address(pool0), address(pool1));
+      l.period = 1 days;
+      l.window = 28;
+    }
   }
 }
