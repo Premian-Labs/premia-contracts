8c93f62949ad50c02af2007bc9ab0f850b4ea153
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-09 19:46:36 -0500

    fix getVariance NatSpec comments

--- contracts/pair/IPair.sol
+++ contracts/pair/IPair.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 
 interface IPair {
   /**
-   * @notice calculate or get cached variance for current day
-   * @return variance64x64 64x64 fixed point representation of variance
+   * @notice calculate or get cached variance for current period
+   * @return variance64x64 64x64 fixed point representation of EMA of annualized variance
    */
   function getVariance () external view returns (int128 variance64x64);
 }
