0ce416024acbb00b1c235c1153a11ed1b1b211ca
Zimri Leijen <zimmah@gmail.com>
2021-04-10 21:31:00 +0200

    partial update implementation

--- contracts/pair/Pair.sol
+++ contracts/pair/Pair.sol
@@ -33,38 +33,46 @@ contract Pair is IPair, OwnableInternal {
     variance64x64 = PairStorage.layout().emaVarianceAnnualized64x64;
   }
 
-  /**
+ /**
    * @notice updates state variables
    */
   function update() internal {
     PairStorage.Layout storage l = PairStorage.layout();
 
     // TODO: skip if trivial amount of time has passed since last update
+    if (l.lasttimestamp + l.period >= block.timestamp) return;
+
+    uint today = block.timestamp / 86400;
+    uint lastDay = l.lasttimestamp / 86400;
+    (roundId, newPrice64x64) = IPriceConsumer(OwnableStorage.layout().owner).getLatestPrice(l.oracle);
+
     // TODO: skip if retrieved round ID is same as last round ID
+    if(l.dayToRoundId[lastDay] == roundId) return;
 
-    (
-      l.oldPrice64x64,
-      l.newPrice64x64
-    ) = (
-      l.newPrice64x64,
-      IPriceConsumer(OwnableStorage.layout().owner).getLatestPrice(l.oracle)
-    );
+    l.dayToRoundId[today] = roundId;
+    
+    if(today == lastDay){
+      l.dayToClosingPrice64x64[today] = newPrice64x64;
+    } else {
+      l.dayToOpeningPrice64x64[today] = newPrice64x64;
+      // TODO: perform binary search to find and store actual close (both price and roundId)
 
-    int128 logreturns64x64 = OptionMath.logreturns(l.newPrice64x64, l.oldPrice64x64);
+      int128 logreturns64x64 = OptionMath.logreturns(l.dayToClosingPrice64x64[today], l.dayToClosingPrice64x64[lastDay]);
 
-    (
-      l.oldEmaLogReturns64x64,
-      l.newEmaLogReturns64x64
-    ) = (
-      l.newEmaLogReturns64x64,
-      OptionMath.rollingEma(l.oldEmaLogReturns64x64, logreturns64x64, l.window)
-    );
+      (
+        l.oldEmaLogReturns64x64,
+        l.newEmaLogReturns64x64
+      ) = (
+        l.newEmaLogReturns64x64,
+        OptionMath.rollingEma(l.oldEmaLogReturns64x64, logreturns64x64, l.window)
+      );
 
-    l.emaVarianceAnnualized64x64 = OptionMath.rollingEmaVariance(
-      l.emaVarianceAnnualized64x64 / 365,
-      l.oldEmaLogReturns64x64,
-      logreturns64x64,
-      l.window
-    ) * 365;
+      l.emaVarianceAnnualized64x64 = OptionMath.rollingEmaVariance(
+        l.emaVarianceAnnualized64x64 / 365,
+        l.oldEmaLogReturns64x64,
+        logreturns64x64,
+        l.window
+      ) * 365;
+    }
   }
 }
