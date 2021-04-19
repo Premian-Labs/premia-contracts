75169b05126930bc36e1d102a8fe44e246cde16d
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-18 22:21:58 -0500

    move price storage to Pair

--- contracts/pair/PairStorage.sol
+++ contracts/pair/PairStorage.sol
@@ -21,6 +21,14 @@ library PairStorage {
     uint256 updatedAt;
 
     int128 emaVarianceAnnualized64x64;
+
+    // TODO: enforced interval size for maturity (maturity % interval == 0)
+    // updatable by owner
+
+    // minimum resolution price bucket => price
+    mapping (uint256 => int128) bucketPrices64x64;
+    // sequence id (minimum resolution price bucket / 256) => price update sequence
+    mapping (uint256 => uint256) priceUpdateSequences;
   }
 
   function layout () internal pure returns (Layout storage l) {
@@ -55,5 +63,57 @@ library PairStorage {
   ) internal {
     l.pool0 = pool0;
     l.pool1 = pool1;
+  }
+
+  function setPriceUpdate (
+    Layout storage l,
+    uint timestamp,
+    int128 price64x64
+  ) internal {
+    // TODO: check for off-by-one errors
+    uint bucket = timestamp / (1 hours);
+    l.bucketPrices64x64[bucket] = price64x64;
+    l.priceUpdateSequences[bucket >> 8] += 1 << 256 - (bucket & 255);
+  }
+
+  function getPriceUpdate (
+    Layout storage l,
+    uint timestamp
+  ) internal view returns (int128) {
+    return l.bucketPrices64x64[timestamp / (1 hours)];
+  }
+
+  function getPriceUpdateAfter (
+    Layout storage l,
+    uint timestamp
+  ) internal view returns (int128) {
+    // TODO: check for off-by-one errors
+    uint bucket = timestamp / (1 hours);
+    uint sequenceId = bucket >> 8;
+    // shift to skip buckets from earlier in sequence
+    uint offset = bucket & 255;
+    uint sequence = l.priceUpdateSequences[sequenceId] << offset >> offset;
+
+    uint currentPriceUpdateSequenceId = block.timestamp / (256 hours);
+
+    while (sequence == 0 && sequenceId <= currentPriceUpdateSequenceId) {
+      sequence = l.priceUpdateSequences[++sequenceId];
+    }
+
+    if (sequence == 0) {
+      // TODO: no price update found; continuing function will return 0 anyway
+      return 0;
+    }
+
+    uint256 msb; // most significant bit
+
+    for (uint256 i = 128; i > 0; i >> 1) {
+      if (sequence >> i > 0) {
+        msb += i;
+        sequence >>= i;
+      }
+    }
+
+    return l.bucketPrices64x64[(sequenceId + 1 << 8) - msb];
   }
 }
--- contracts/pool/PoolStorage.sol
+++ contracts/pool/PoolStorage.sol
@@ -15,70 +15,10 @@ library PoolStorage {
     uint8 underlyingDecimals;
     int128 liquidity64x64;
     int128 cLevel64x64;
-
-    // TODO: enforced interval size for maturity (maturity % interval == 0)
-    // updatable by owner
-
-    // minimum resolution price bucket => price
-    mapping (uint256 => int128) bucketPrices64x64;
-    // sequence id (minimum resolution price bucket / 256) => price update sequence
-    mapping (uint256 => uint256) priceUpdateSequences;
   }
 
   function layout () internal pure returns (Layout storage l) {
     bytes32 slot = STORAGE_SLOT;
     assembly { l.slot := slot }
-  }
-
-  function setPriceUpdate (
-    Layout storage l,
-    uint timestamp,
-    int128 price64x64
-  ) internal {
-    // TODO: check for off-by-one errors
-    uint bucket = timestamp / (1 hours);
-    l.bucketPrices64x64[bucket] = price64x64;
-    l.priceUpdateSequences[bucket >> 8] += 1 << 256 - (bucket & 255);
-  }
-
-  function getPriceUpdate (
-    Layout storage l,
-    uint timestamp
-  ) internal view returns (int128) {
-    return l.bucketPrices64x64[timestamp / (1 hours)];
-  }
-
-  function getPriceUpdateAfter (
-    Layout storage l,
-    uint timestamp
-  ) internal view returns (int128) {
-    // TODO: check for off-by-one errors
-    uint bucket = timestamp / (1 hours);
-    uint sequenceId = bucket >> 8;
-    // shift to skip buckets from earlier in sequence
-    uint offset = bucket & 255;
-    uint sequence = l.priceUpdateSequences[sequenceId] << offset >> offset;
-
-    uint currentPriceUpdateSequenceId = block.timestamp / (256 hours);
-
-    while (sequence == 0 && sequenceId <= currentPriceUpdateSequenceId) {
-      sequence = l.priceUpdateSequences[++sequenceId];
-    }
-
-    if (sequence == 0) {
-      // TODO: no price update found; continuing function will return 0 anyway
-      return 0;
-    }
-
-    uint256 msb; // most significant bit
-
-    for (uint256 i = 128; i > 0; i >> 1) {
-      if (sequence >> i > 0) {
-        msb += i;
-        sequence >>= i;
-      }
-    }
-
-    return l.bucketPrices64x64[(sequenceId + 1 << 8) - msb];
   }
 }
