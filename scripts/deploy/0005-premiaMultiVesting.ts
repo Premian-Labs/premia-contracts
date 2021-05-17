46522edb62f998ed64f6c248e3d2e03bb2b73037
Lumyo <lumyo.b@gmail.com>
2021-05-17 16:32:25 +0800

    PremiaMultiVesting deployment script

---
+++ scripts/deploy/0005-premiaMultiVesting.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'hardhat';
+import {PremiaMultiVesting__factory} from '../../contractsTyped';
+
+// This will need to be deployed after end of PBC, in order to know start price of the bonding curve which will be the final price of the PBC
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const premia = '0x6399C842dD2bE3dE30BF99Bc7D1bBF6Fa3650E70';
+  const treasury = '0xc22FAe86443aEed038A4ED887bbA8F5035FD12F0';
+
+  const premiaMultiVesting = await new PremiaMultiVesting__factory(deployer).deploy(
+    premia,
+  );
+
+  console.log(
+    `PremiaMultiVesting deployed at ${premiaMultiVesting.address} (Args : ${premia} )`,
+  );
+
+  await premiaMultiVesting.transferOwnership(treasury);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
