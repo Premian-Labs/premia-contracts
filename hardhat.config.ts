8f799c5f32815a87797dcec379bf4b7de52d28f3
froggiedev <froggiedev@protonmail.com>
2021-06-01 20:53:31 -1000

    Merge typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import Dotenv from 'dotenv';
+
 Dotenv.config();
 
 import '@nomiclabs/hardhat-waffle';
@@ -10,6 +11,9 @@ import 'solidity-coverage';
 
 import './tasks/deploy';
 import './tasks/accounts';
+
+const ETH_TEST_KEY = process.env.ETH_TEST_PKEY;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 
 export default {
   solidity: {
@@ -28,6 +32,13 @@ export default {
       accounts: {
         mnemonic: process.env.MNEMONIC,
       },
+    },
+
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [ETH_TEST_KEY],
+      blockGasLimit: 120000000000,
+      timeout: 300000,
     },
   },
 
--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,5 +1,10 @@
 import { task } from 'hardhat/config';
 
+const RINKEBY_DAI = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e';
+const RINKEBY_WETH = '0xc778417e063141139fce010982780140aa0cd5ab';
+const RINKEBY_DAI_PRICE_ORACLE = '0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF';
+const RINKEBY_ETH_PRICE_ORACLE = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e';
+
 task('deploy').setAction(async function (args, hre) {
   const {
     Median__factory,
@@ -27,10 +32,16 @@ task('deploy').setAction(async function (args, hre) {
     },
   );
 
-  const instance = await new Median__factory(deployer).deploy(
+  const median = await new Median__factory(deployer).deploy(
     pair.address,
     pool.address,
   );
 
-  await instance.diamondCut(facetCuts, hre.ethers.constants.AddressZero, '0x');
+  await median.diamondCut(facetCuts, hre.ethers.constants.AddressZero, '0x');
+
+  await ProxyManager__factory.connect(median.address, deployer).deployPair(RINKEBY_DAI, RINKEBY_WETH, RINKEBY_DAI_PRICE_ORACLE, RINKEBY_ETH_PRICE_ORACLE);
+
+  console.log('Deployer: ', deployer.address);
+  console.log('Pool: ', pool.address);
+  console.log('Median: ', median.address);
 });
