9eeff57a8c39565f6cf1de898df7da59c0075dae
froggiedev <froggiedev@protonmail.com>
2021-06-04 12:23:07 -1000

    Update deploy script for rinkeby

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,19 +1,21 @@
 import { task } from 'hardhat/config';
+import {
+  Premia__factory,
+  Pair__factory,
+  Pool__factory,
+  ProxyManager__factory,
+} from '../typechain';
+
+const RINKEBY_DAI = '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea';
+const RINKEBY_WETH = '0xc778417e063141139fce010982780140aa0cd5ab';
+const RINKEBY_DAI_PRICE_ORACLE = '0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF';
+const RINKEBY_ETH_PRICE_ORACLE = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e';
 
 task('deploy').setAction(async function (args, hre) {
-  const {
-    Premia__factory,
-    Pair__factory,
-    Pool__factory,
-    ProxyManager__factory,
-  } = require('../typechain');
-
   const [deployer] = await hre.ethers.getSigners();
 
   const pair = await new Pair__factory(deployer).deploy();
-  const pool = await new Pool__factory(deployer).deploy(
-    hre.ethers.constants.AddressZero,
-  );
+  const pool = await new Pool__factory(deployer).deploy(RINKEBY_WETH);
 
   const facetCuts = [await new ProxyManager__factory(deployer).deploy()].map(
     function (f) {
@@ -27,10 +29,27 @@ task('deploy').setAction(async function (args, hre) {
     },
   );
 
-  const instance = await new Premia__factory(deployer).deploy(
+  const median = await new Premia__factory(deployer).deploy(
     pair.address,
     pool.address,
   );
 
-  await instance.diamondCut(facetCuts, hre.ethers.constants.AddressZero, '0x');
+  const tx = await median.diamondCut(
+    facetCuts,
+    hre.ethers.constants.AddressZero,
+    '0x',
+  );
+
+  await tx.wait(1);
+
+  await ProxyManager__factory.connect(median.address, deployer).deployPair(
+    RINKEBY_DAI,
+    RINKEBY_WETH,
+    RINKEBY_DAI_PRICE_ORACLE,
+    RINKEBY_ETH_PRICE_ORACLE,
+  );
+
+  console.log('Deployer: ', deployer.address);
+  console.log('Pool: ', pool.address);
+  console.log('Median: ', median.address);
 });
