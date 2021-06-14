beaf58fac9a740b874df2405427e70c156b3c02f
Lumyo <lumyo.b@gmail.com>
2021-06-14 18:19:18 +0800

    Use setOracles in PoolProxy constructor

--- contracts/pool/PoolProxy.sol
+++ contracts/pool/PoolProxy.sol
@@ -38,8 +38,7 @@ contract PoolProxy is ManagedProxyOwnable {
       l.base = base;
       l.underlying = underlying;
 
-      l.baseOracle = baseOracle;
-      l.underlyingOracle = underlyingOracle;
+      l.setOracles(baseOracle, underlyingOracle);
 
       l.underlyingDecimals = IERC20Metadata(underlying).decimals();
       l.cLevel64x64 = OptionMath.INITIAL_C_LEVEL_64x64;
