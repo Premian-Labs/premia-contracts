b40cd9a659849e8c61840dd5e68d477278b9d9df
Nick Barry <itsnickbarry@protonmail.ch>
2021-06-22 15:00:46 -0500

    increase slippage tolerance for purchase tests

--- test/pool/PoolProxy.ts
+++ test/pool/PoolProxy.ts
@@ -103,14 +103,14 @@ describe('PoolProxy', function () {
       return parseUnderlying(
         (
           (fixedToNumber(baseCost64x64) + fixedToNumber(feeCost64x64)) *
-          1.02
+          1.03
         ).toString(),
       );
     } else {
       return parseBase(
         (
           (fixedToNumber(baseCost64x64) + fixedToNumber(feeCost64x64)) *
-          1.02
+          1.03
         ).toString(),
       );
     }
