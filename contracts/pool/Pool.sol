981ab3c273e708befdff3897376305fe0b115e3e
Lumyo <lumyo.b@gmail.com>
2021-06-29 13:57:44 +0800

    Deduplicate balanceOf calls

--- contracts/pool/Pool.sol
+++ contracts/pool/Pool.sol
@@ -903,10 +903,13 @@ contract Pool is OwnableInternal, ERC1155Enumerable {
         if (amount > 0) {
           bool isCallPool = id == UNDERLYING_FREE_LIQ_TOKEN_ID;
 
+          if (to != address(0)) {
+            uint256 balance = balanceOf(from, id);
           // ToDo : Find better solution than checking if under 1e5 to ignore dust left ?
-          if (from != address(0) && balanceOf(from, id) - amount <= 1e5) {
-            require(balanceOf(from, id) - l.pendingDeposits[from][l.nextDeposits[isCallPool].eta][isCallPool] >= amount, 'Insuf balance');
-            l.removeUnderwriter(from, isCallPool);
+            if (from != address(0) && balance <= amount + 1e5) {
+              require(balance - l.pendingDeposits[from][l.nextDeposits[isCallPool].eta][isCallPool] >= amount, 'Insuf balance');
+              l.removeUnderwriter(from, isCallPool);
+            }
           }
 
           if (to != address(0)) {
