ac5b68b188ab1821fe9c88fa05aae51dd169f8fa
nikkatalnikov <nk@flowtale.ai>
2021-07-15 22:48:58 +0300

    OptionMath.ts _decay: add test case for steepness 0,5

--- test/libraries/OptionMath.ts
+++ test/libraries/OptionMath.ts
@@ -225,7 +225,7 @@ describe('OptionMath', function () {
   });
 
   describe('#quotePrice', function () {
-    it('calculates European CALL option price quote ', async function () {
+    it('calculates European CALL option price quote at steepness 1', async function () {
       const variance = fixedFromFloat(0.16);
       const price = input_t[1];
       const strike = fixedFromFloat(55284 * 0.95);
@@ -237,6 +237,37 @@ describe('OptionMath', function () {
 
       const expected = bnToNumber(
         fixedFromFloat(2.2255409285 * 4013.677084809402 * 1.5319261606),
+      ); // c * bsch * slippage
+      const result = bnToNumber(
+        (
+          await instance.callStatic.quotePrice({
+            emaVarianceAnnualized64x64: variance,
+            strike64x64: strike,
+            spot64x64: price,
+            timeToMaturity64x64: maturity,
+            oldCLevel64x64: cLevel,
+            oldPoolState: S0,
+            newPoolState: S1,
+            steepness64x64: steepness,
+            isCall: true,
+          })
+        )[0],
+      );
+
+      expect(expected / result).to.be.closeTo(1, 0.001);
+    });
+    it('calculates European CALL option price quote at steepness 0.5', async function () {
+      const variance = fixedFromFloat(0.16);
+      const price = input_t[1];
+      const strike = fixedFromFloat(55284 * 0.95);
+      const maturity = fixedFromFloat(28 / 365);
+      const cLevel = fixedFromFloat(1);
+      const S0 = fixedFromFloat(100);
+      const S1 = fixedFromFloat(20);
+      const steepness = fixedFromFloat(0.5);
+
+      const expected = bnToNumber(
+        fixedFromFloat(1.4918246976 * 4013.677084809402 * 1.2295617441),
       ); // c * bsch * slippage
       const result = bnToNumber(
         (
