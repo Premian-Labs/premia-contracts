f29327cfa09916c9bc17458ee2106cb4dda12281
Lumyo <lumyo.b@gmail.com>
2021-07-19 14:32:30 +0800

    Clean old unused contracts

--- contracts/old/core/IProxyManagerOld.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.0;
-
-interface IProxyManagerOld {
-    function getOptionImplementation() external view returns (address);
-
-    function getMarketImplementation() external view returns (address);
-}
--- scripts/utils/deployV1.ts
+++ scripts/utils/deployV1.ts
@@ -1,31 +1,17 @@
 import {
-  ERC20,
-  ERC20__factory,
   ERC20Mock,
   ERC20Mock__factory,
-  FeeCalculator,
-  FeeCalculator__factory,
-  Market,
-  Market__factory,
-  Option,
-  Option__factory,
-  Premia,
   PremiaErc20,
   PremiaErc20__factory,
   PremiaFeeDiscount,
   PremiaFeeDiscount__factory,
   PremiaMaker,
   PremiaMaker__factory,
-  PremiaOld__factory,
-  PremiaOptionBatch,
-  PremiaOptionBatch__factory,
   PremiaStaking,
   PremiaStaking__factory,
-  ProxyManagerOld__factory,
   ProxyUpgradeableOwnable__factory,
 } from '../../typechain';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
-import { ZERO_ADDRESS } from '../../test/utils/constants';
 
 export async function deployV1(
   deployer: SignerWithAddress,
@@ -36,19 +22,12 @@ export async function deployV1(
   daiAddress?: string,
 ): Promise<IPremiaContracts> {
   let premia: PremiaErc20 | ERC20Mock;
-  let dai: ERC20 | ERC20Mock;
 
   if (isTest) {
     if (!premiaAddress) {
       premia = await new ERC20Mock__factory(deployer).deploy('PREMIA', 18);
     } else {
       premia = PremiaErc20__factory.connect(premiaAddress, deployer);
-    }
-
-    if (!daiAddress) {
-      dai = await new ERC20Mock__factory(deployer).deploy('DAI', 18);
-    } else {
-      dai = ERC20__factory.connect(daiAddress, deployer);
     }
   } else {
     if (!premiaAddress) {
@@ -59,7 +38,6 @@ export async function deployV1(
     }
     // premia = await new PremiaErc20__factory(deployer).deploy();
     premia = PremiaErc20__factory.connect(premiaAddress, deployer);
-    dai = ERC20__factory.connect(daiAddress, deployer);
   }
 
   if (log) {
@@ -107,102 +85,17 @@ export async function deployV1(
     );
   }
 
-  const feeCalculator = await new FeeCalculator__factory(deployer).deploy(
-    premiaFeeDiscount.address,
-  );
-  if (log) {
-    console.log(
-      `FeeCalculator deployed at ${feeCalculator.address} (Args : ${premiaFeeDiscount.address})`,
-    );
-  }
-
-  const premiaOptionBatch = await new PremiaOptionBatch__factory(
-    deployer,
-  ).deploy();
-  if (log) {
-    console.log(`PremiaOptionBatch deployed at ${premiaOptionBatch.address}`);
-  }
-
-  ///
-
-  // Diamond
-  const optionImpl = await new Option__factory(deployer).deploy();
-  const marketImpl = await new Market__factory(deployer).deploy();
-
-  const premiaDiamond = await new PremiaOld__factory(deployer).deploy(
-    optionImpl.address,
-    marketImpl.address,
-  );
-
-  const facetCuts = [await new ProxyManagerOld__factory(deployer).deploy()].map(
-    (f) => {
-      return {
-        target: f.address,
-        action: 0,
-        selectors: Object.keys(f.interface.functions)
-          .filter(
-            (fn) => !['owner()', 'transferOwnership(address)'].includes(fn),
-          )
-          .map((fn) => {
-            // console.log(fn);
-            return f.interface.getSighash(fn);
-          }),
-      };
-    },
-  );
-
-  await premiaDiamond.diamondCut(facetCuts, ZERO_ADDRESS, '0x');
-  const proxyManager = ProxyManagerOld__factory.connect(
-    premiaDiamond.address,
-    deployer,
-  );
-
-  await proxyManager.deployOption(
-    '',
-    dai.address,
-    feeCalculator.address,
-    treasury,
-  );
-  await proxyManager.deployMarket(feeCalculator.address, treasury);
-
-  // console.log(await premiaDiamond.owner());
-  // console.log(premiaDiamond.address, deployer.address);
-
-  const option = Option__factory.connect(
-    await proxyManager.getOption(dai.address),
-    deployer,
-  );
-  const market = Market__factory.connect(
-    await proxyManager.getMarket(),
-    deployer,
-  );
-
-  // console.log('Option owner : ', await option.owner());
-  // console.log('Market owner : ', await market.owner());
-
   return {
-    premiaDiamond,
-    option,
-    market,
     premia,
-    dai,
     premiaMaker,
     xPremia,
     premiaFeeDiscount,
-    feeCalculator,
-    premiaOptionBatch,
   };
 }
 
 export interface IPremiaContracts {
-  premiaDiamond: Premia;
-  option: Option;
-  market: Market;
   premia: PremiaErc20 | ERC20Mock;
-  dai: ERC20 | ERC20Mock;
   xPremia: PremiaStaking;
   premiaFeeDiscount: PremiaFeeDiscount;
   premiaMaker: PremiaMaker;
-  feeCalculator: FeeCalculator;
-  premiaOptionBatch: PremiaOptionBatch;
 }
