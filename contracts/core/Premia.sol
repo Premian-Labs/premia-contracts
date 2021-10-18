0c086c3dca3d5bca0b60d545ddc8e94ab4766ca9
Nick Barry <itsnickbarry@protonmail.ch>
2021-10-18 01:47:48 -0500

    remove redundant contract owner setter

--- contracts/core/Premia.sol
+++ contracts/core/Premia.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import {OwnableStorage} from "@solidstate/contracts/access/OwnableStorage.sol";
 import {Diamond} from "@solidstate/contracts/proxy/diamond/Diamond.sol";
 
 /**
@@ -10,7 +9,5 @@ import {Diamond} from "@solidstate/contracts/proxy/diamond/Diamond.sol";
  * @dev based on the EIP2535 Diamond standard
  */
 contract Premia is Diamond {
-    constructor() {
-        OwnableStorage.layout().owner = msg.sender;
-    }
+
 }
