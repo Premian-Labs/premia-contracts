e225dd7be0a592ec36cbf7db1df894b015bfe3f0
Lumyo <lumyo.b@gmail.com>
2021-10-27 18:35:50 +0800

    Add ERC1155 token name

--- contracts/pool/IPoolBase.sol
+++ contracts/pool/IPoolBase.sol
@@ -6,4 +6,6 @@ import {IERC1155} from "@solidstate/contracts/token/ERC1155/IERC1155.sol";
 import {IERC1155Enumerable} from "@solidstate/contracts/token/ERC1155/enumerable/IERC1155Enumerable.sol";
 import {IMulticall} from "@solidstate/contracts/utils/IMulticall.sol";
 
-interface IPoolBase is IERC1155, IERC1155Enumerable, IMulticall {}
+interface IPoolBase is IERC1155, IERC1155Enumerable, IMulticall {
+    function name() external view returns (string memory);
+}
