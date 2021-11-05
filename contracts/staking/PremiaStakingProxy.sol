0f9741865473dd7c1744792b1c1d3d8903579a39
Nick Barry <itsnickbarry@protonmail.ch>
2021-11-05 16:22:22 -0600

    use ProxyUpgradeableOwnable for PremiaStakingProxy

--- contracts/staking/PremiaStakingProxy.sol
+++ contracts/staking/PremiaStakingProxy.sol
@@ -2,13 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import {Diamond} from "@solidstate/contracts/proxy/diamond/Diamond.sol";
+import {ProxyUpgradeableOwnable} from "../ProxyUpgradeableOwnable.sol";
 import {ERC20MetadataStorage} from "@solidstate/contracts/token/ERC20/metadata/ERC20MetadataStorage.sol";
 
-contract PremiaStakingProxy is Diamond {
+contract PremiaStakingProxy is ProxyUpgradeableOwnable {
     using ERC20MetadataStorage for ERC20MetadataStorage.Layout;
 
-    constructor() {
+    constructor(address implementation)
+        ProxyUpgradeableOwnable(implementation)
+    {
         ERC20MetadataStorage.Layout storage l = ERC20MetadataStorage.layout();
         l.setName("PremiaStaking");
         l.setSymbol("xPremia");
