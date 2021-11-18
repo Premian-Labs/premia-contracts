b7f444134e4b7d0ce0434b4fb956ff4f30c479e1
Lumyo <lumyo.b@gmail.com>
2021-11-18 12:33:12 +0800

    Add tests for withdrawal delay

--- test/staking/PremiaStaking.test.ts
+++ test/staking/PremiaStaking.test.ts
@@ -39,6 +39,7 @@ describe('PremiaStaking', () => {
       admin,
     );
 
+    await premia.mint(admin.address, '1000');
     await premia.mint(alice.address, '100');
     await premia.mint(bob.address, '100');
     await premia.mint(carol.address, '100');
@@ -88,50 +89,115 @@ describe('PremiaStaking', () => {
     ).to.be.revertedWith('ERC20: burn amount exceeds balance');
   });
 
+  it('should correctly handle withdrawal with delay', async () => {
+    await premia.connect(alice).approve(premiaStaking.address, '100');
+    await premiaStaking.connect(alice).deposit('100');
+
+    await expect(premiaStaking.connect(alice).withdraw()).to.be.revertedWith(
+      'No pending withdrawal',
+    );
+
+    await premiaStaking.connect(alice).startWithdraw('40');
+
+    expect(await premiaStaking.getStakedPremiaAmount()).to.eq('60');
+
+    await increaseTimestamp(ONE_DAY * 10 - 5);
+    await expect(premiaStaking.connect(alice).withdraw()).to.be.revertedWith(
+      'Withdrawal still pending',
+    );
+
+    await increaseTimestamp(10);
+
+    await premiaStaking.connect(alice).withdraw();
+    expect(await premiaStaking.balanceOf(alice.address)).to.eq('60');
+    expect(await premia.balanceOf(alice.address)).to.eq('40');
+
+    await expect(premiaStaking.connect(alice).withdraw()).to.be.revertedWith(
+      'No pending withdrawal',
+    );
+  });
+
   it('should work with more than one participant', async () => {
     await premia.connect(alice).approve(premiaStaking.address, '100');
     await premia.connect(bob).approve(premiaStaking.address, '100');
+    await premia.connect(carol).approve(premiaStaking.address, '100');
 
     // Alice enters and gets 20 shares. Bob enters and gets 10 shares.
-    await premiaStaking.connect(alice).deposit('20');
+    await premiaStaking.connect(alice).deposit('30');
     await premiaStaking.connect(bob).deposit('10');
+    await premiaStaking.connect(carol).deposit('10');
 
     let aliceBalance = await premiaStaking.balanceOf(alice.address);
     let bobBalance = await premiaStaking.balanceOf(bob.address);
+    let carolBalance = await premiaStaking.balanceOf(carol.address);
     let contractBalance = await premia.balanceOf(premiaStaking.address);
 
-    expect(aliceBalance).to.eq(20);
+    expect(aliceBalance).to.eq(30);
     expect(bobBalance).to.eq(10);
-    expect(contractBalance).to.eq(30);
+    expect(carolBalance).to.eq(10);
+    expect(contractBalance).to.eq(50);
 
     // PremiaStaking get 20 more PREMIAs from an external source.
-    await premia.connect(carol).transfer(premiaStaking.address, '20');
+    await premia.connect(admin).transfer(premiaStaking.address, '50');
 
-    // Alice deposits 10 more PREMIAs. She should receive 10*30/50 = 6 shares.
-    await premiaStaking.connect(alice).deposit('10');
+    // Bob deposits 50 more PREMIAs. She should receive 50*50/100 = 25 shares.
+    await premiaStaking.connect(bob).deposit('50');
+
+    expect(await premiaStaking.getXPremiaToPremiaRatio()).to.eq(2);
+
     aliceBalance = await premiaStaking.balanceOf(alice.address);
     bobBalance = await premiaStaking.balanceOf(bob.address);
+    carolBalance = await premiaStaking.balanceOf(carol.address);
 
-    expect(aliceBalance).to.eq(26);
-    expect(bobBalance).to.eq(10);
+    expect(aliceBalance).to.eq(30);
+    expect(bobBalance).to.eq(35);
+    expect(carolBalance).to.eq(10);
 
-    // Bob withdraws 5 shares. He should receive 5*60/36 = 8 shares
-    await premiaStaking.connect(bob).startWithdraw('5');
+    await premiaStaking.connect(alice).startWithdraw('5');
+    await premiaStaking.connect(bob).startWithdraw('20');
+
+    aliceBalance = await premiaStaking.balanceOf(alice.address);
+    bobBalance = await premiaStaking.balanceOf(bob.address);
+    carolBalance = await premiaStaking.balanceOf(carol.address);
+
+    expect(aliceBalance).to.eq(25);
+    expect(bobBalance).to.eq(15);
+    expect(carolBalance).to.eq(10);
+
+    // Pending withdrawals should not count anymore as staked
+    await premia.connect(admin).transfer(premiaStaking.address, '100');
+
+    expect(await premiaStaking.getXPremiaToPremiaRatio()).to.eq(4);
+
     await increaseTimestamp(10 * ONE_DAY + 1);
+    await premiaStaking.connect(alice).withdraw();
     await premiaStaking.connect(bob).withdraw();
 
-    aliceBalance = await premiaStaking.balanceOf(alice.address);
-    bobBalance = await premiaStaking.balanceOf(bob.address);
-    contractBalance = await premia.balanceOf(premiaStaking.address);
+    let alicePremiaBalance = await premia.balanceOf(alice.address);
+    let bobPremiaBalance = await premia.balanceOf(bob.address);
 
-    expect(aliceBalance).to.eq(26);
-    expect(bobBalance).to.eq(5);
-    expect(contractBalance).to.eq(52);
+    // Alice = 100 - 30 + (5 * 2)
+    expect(alicePremiaBalance).to.eq(80);
+    // Bob = 100 - 10 - 50 + 40
+    expect(bobPremiaBalance).to.eq(80);
 
-    const alicePremiaBalance = await premia.balanceOf(alice.address);
-    const bobPremiaBalance = await premia.balanceOf(bob.address);
+    await premiaStaking.connect(alice).startWithdraw('25');
+    await premiaStaking.connect(bob).startWithdraw('15');
+    await premiaStaking.connect(carol).startWithdraw('10');
 
-    expect(alicePremiaBalance).to.eq(70);
-    expect(bobPremiaBalance).to.eq(98);
+    await increaseTimestamp(10 * ONE_DAY + 1);
+
+    await premiaStaking.connect(alice).withdraw();
+    await premiaStaking.connect(bob).withdraw();
+    await premiaStaking.connect(carol).withdraw();
+
+    alicePremiaBalance = await premia.balanceOf(alice.address);
+    bobPremiaBalance = await premia.balanceOf(bob.address);
+    const carolPremiaBalance = await premia.balanceOf(carol.address);
+
+    expect(await premiaStaking.totalSupply()).to.eq(0);
+    expect(alicePremiaBalance).to.eq(180);
+    expect(bobPremiaBalance).to.eq(140);
+    expect(carolPremiaBalance).to.eq(130);
   });
 });
