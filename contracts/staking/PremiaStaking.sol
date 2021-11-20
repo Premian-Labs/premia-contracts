f9dfefaa5d2bb8e8439e06d918db4ef3f2151110
Nick Barry <itsnickbarry@protonmail.ch>
2021-11-19 19:33:20 -0600

    remove PremiaStaking openzeppelin imports

--- contracts/staking/PremiaStaking.sol
+++ contracts/staking/PremiaStaking.sol
@@ -3,11 +3,11 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@solidstate/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@solidstate/contracts/token/ERC20/ERC20.sol";
 import {IERC2612} from "@solidstate/contracts/token/ERC20/permit/IERC2612.sol";
 import {ERC20Permit} from "@solidstate/contracts/token/ERC20/permit/ERC20Permit.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20} from "@solidstate/contracts/utils/SafeERC20.sol";
 
 import "./IPremiaStaking.sol";
 
