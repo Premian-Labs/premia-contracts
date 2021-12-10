9d6870dd338c7cb9be1ce4b6ac50487ec2d41e60
Lumyo <lumyo.b@gmail.com>
2021-12-10 14:57:54 +0800

    Update rinkeby script

--- scripts/deploy/testnet/0004-deployV2Rinkeby.ts
+++ scripts/deploy/testnet/0004-deployV2Rinkeby.ts
@@ -1,7 +1,6 @@
 import { deployV2, TokenAddresses, TokenAmounts } from '../../utils/deployV2';
 import { fixedFromFloat } from '@premia/utils';
 import {
-  ERC20Mock__factory,
   PremiaErc20__factory,
   PremiaMakerKeeper__factory,
   ProcessExpiredKeeper__factory,
@@ -26,16 +25,16 @@ async function main() {
     '0xc778417e063141139fce010982780140aa0cd5ab',
   );
 
-  const eth = await new ERC20Mock__factory(deployer).deploy('ETH', 18);
-  const dai = await new ERC20Mock__factory(deployer).deploy('DAI', 18);
-  const wbtc = await new ERC20Mock__factory(deployer).deploy('WBTC', 8);
-  const link = await new ERC20Mock__factory(deployer).deploy('LINK', 18);
+  // const eth = await new ERC20Mock__factory(deployer).deploy('ETH', 18);
+  // const dai = await new ERC20Mock__factory(deployer).deploy('DAI', 18);
+  // const wbtc = await new ERC20Mock__factory(deployer).deploy('WBTC', 8);
+  // const link = await new ERC20Mock__factory(deployer).deploy('LINK', 18);
 
   const tokens: TokenAddresses = {
-    ETH: eth.address,
-    DAI: dai.address,
-    BTC: wbtc.address,
-    LINK: link.address,
+    ETH: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    DAI: '0xe7ED2AE5fc5E04ba3A9E04fD4a46D210F73990e6',
+    BTC: '0xF4A41f753A09BE3DdCB82856808e4B8c44e36f6D',
+    LINK: '0xd6620e82b200a6486D419c3ea9D840fC811D86be',
   };
 
   // Rinkeby addresses
@@ -54,23 +53,24 @@ async function main() {
   };
 
   const caps: TokenAmounts = {
-    DAI: '1000000',
-    ETH: '300',
-    BTC: '25',
-    LINK: '40000',
+    DAI: '10000000',
+    ETH: '3000',
+    BTC: '250',
+    LINK: '400000',
   };
 
   const premiaDiamond = await deployV2(
     weth,
     premia.address,
-    fixedFromFloat(0.01),
+    fixedFromFloat(0.03),
     contracts.premiaMaker.address,
-    contracts.premiaFeeDiscount.address,
+    contracts.xPremia.address,
     tokens,
     oracles,
     minimums,
     caps,
     '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+    '0x9e88fe5e5249CD6429269B072c9476b6908dCBf2',
   );
 
   const premiaMakerKeeper = await new PremiaMakerKeeper__factory(
