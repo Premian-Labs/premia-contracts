d28be26e745624cdb5c34fdf02f33378b737d930
Nick Barry <itsnickbarry@protonmail.ch>
2022-02-14 02:27:33 -0600

    add _verifyPurchase function

--- contracts/pool/PoolWrite.sol
+++ contracts/pool/PoolWrite.sol
@@ -275,25 +275,9 @@ contract PoolWrite is IPoolWrite, PoolSwap {
     ) internal returns (uint256 baseCost, uint256 feeCost) {
         PoolStorage.Layout storage l = PoolStorage.layout();
 
-        require(maturity >= block.timestamp + (1 days), "exp < 1 day");
-        require(maturity < block.timestamp + (91 days), "exp > 90 days");
-        require(maturity % (8 hours) == 0, "exp must be 8-hour increment");
-
         int128 newPrice64x64 = _update(l);
 
-        if (isCall) {
-            require(
-                strike64x64 <= newPrice64x64 << 1 &&
-                    strike64x64 >= (newPrice64x64 * 8) / 10,
-                "strike out of range"
-            );
-        } else {
-            require(
-                strike64x64 <= (newPrice64x64 * 12) / 10 &&
-                    strike64x64 >= newPrice64x64 >> 1,
-                "strike out of range"
-            );
-        }
+        _verifyPurchase(maturity, strike64x64, isCall, newPrice64x64);
 
         (baseCost, feeCost) = _purchase(
             l,
@@ -315,5 +299,37 @@ contract PoolWrite is IPoolWrite, PoolSwap {
             amount,
             creditMessageValue ? _creditMessageValue(amount, isCall) : 0
         );
+    }
+
+    /**
+     * @notice verify option parameters before writing
+     * @param maturity timestamp of option maturity
+     * @param strike64x64 64x64 fixed point representation of strike price
+     * @param isCall true for call, false for put
+     * @param spot64x64 64x64 fixed point representation of spot price
+     */
+    function _verifyPurchase(
+        uint64 maturity,
+        int128 strike64x64,
+        bool isCall,
+        int128 spot64x64
+    ) internal view {
+        require(maturity >= block.timestamp + (1 days), "exp < 1 day");
+        require(maturity < block.timestamp + (91 days), "exp > 90 days");
+        require(maturity % (8 hours) == 0, "exp must be 8-hour increment");
+
+        if (isCall) {
+            require(
+                strike64x64 <= spot64x64 << 1 &&
+                    strike64x64 >= (spot64x64 * 8) / 10,
+                "strike out of range"
+            );
+        } else {
+            require(
+                strike64x64 <= (spot64x64 * 12) / 10 &&
+                    strike64x64 >= spot64x64 >> 1,
+                "strike out of range"
+            );
+        }
     }
 }
