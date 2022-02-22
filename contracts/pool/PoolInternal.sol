abc71157dc8579b424c8095fb963d566a10e1811
Nick Barry <itsnickbarry@protonmail.ch>
2022-02-21 19:51:34 -0600

    remove redundant token id parsing

--- contracts/pool/PoolInternal.sol
+++ contracts/pool/PoolInternal.sol
@@ -517,12 +517,6 @@ contract PoolInternal is IPoolEvents, ERC1155EnumerableInternal {
             strike64x64
         );
 
-        uint256 shortTokenId = PoolStorage.formatTokenId(
-            PoolStorage.getTokenType(isCall, false),
-            maturity,
-            strike64x64
-        );
-
         _updateCLevelAverage(l, longTokenId, contractSize, quote.cLevel64x64);
 
         // mint long option token for buyer
@@ -533,9 +527,10 @@ contract PoolInternal is IPoolEvents, ERC1155EnumerableInternal {
         _mintShortTokenLoop(
             l,
             account,
+            maturity,
+            strike64x64,
             contractSize,
             baseCost,
-            shortTokenId,
             isCall
         );
         int128 newLiquidity64x64 = l.totalFreeLiquiditySupply64x64(isCall);
@@ -697,13 +692,10 @@ contract PoolInternal is IPoolEvents, ERC1155EnumerableInternal {
 
         _burnShortTokenLoop(
             l,
+            maturity,
+            strike64x64,
             contractSize,
             exerciseValue,
-            PoolStorage.formatTokenId(
-                PoolStorage.getTokenType(isCall, false),
-                maturity,
-                strike64x64
-            ),
             isCall
         );
     }
@@ -737,29 +729,32 @@ contract PoolInternal is IPoolEvents, ERC1155EnumerableInternal {
     function _mintShortTokenLoop(
         PoolStorage.Layout storage l,
         address buyer,
+        uint64 maturity,
+        int128 strike64x64,
         uint256 contractSize,
         uint256 premium,
-        uint256 shortTokenId,
         bool isCall
     ) internal {
-        uint256 tokenAmount;
-        uint256 apyFee;
+        uint256 shortTokenId = PoolStorage.formatTokenId(
+            PoolStorage.getTokenType(isCall, false),
+            maturity,
+            strike64x64
+        );
 
-        {
-            (, uint64 maturity, int128 strike64x64) = PoolStorage.parseTokenId(
-                shortTokenId
-            );
+        uint256 tokenAmount = l.contractSizeToBaseTokenAmount(
+            contractSize,
+            strike64x64,
+            isCall
+        );
 
-            tokenAmount = l.contractSizeToBaseTokenAmount(
-                contractSize,
-                strike64x64,
-                isCall
-            );
+        // calculate anticipated APY fee so that it may be reserved
 
-            // calculate anticipated APY fee so that it may be reserved
-
-            apyFee = _calculateApyFee(l, shortTokenId, tokenAmount, maturity);
-        }
+        uint256 apyFee = _calculateApyFee(
+            l,
+            shortTokenId,
+            tokenAmount,
+            maturity
+        );
 
         while (tokenAmount > 0) {
             address underwriter = l.liquidityQueueAscending[isCall][address(0)];
@@ -942,29 +937,32 @@ contract PoolInternal is IPoolEvents, ERC1155EnumerableInternal {
 
     function _burnShortTokenLoop(
         PoolStorage.Layout storage l,
+        uint64 maturity,
+        int128 strike64x64,
         uint256 contractSize,
         uint256 exerciseValue,
-        uint256 shortTokenId,
         bool isCall
     ) internal {
-        uint256 tokenAmount;
-        uint256 apyFee;
+        uint256 shortTokenId = PoolStorage.formatTokenId(
+            PoolStorage.getTokenType(isCall, false),
+            maturity,
+            strike64x64
+        );
 
-        {
-            (, uint64 maturity, int128 strike64x64) = PoolStorage.parseTokenId(
-                shortTokenId
-            );
+        uint256 tokenAmount = l.contractSizeToBaseTokenAmount(
+            contractSize,
+            strike64x64,
+            isCall
+        );
 
-            tokenAmount = l.contractSizeToBaseTokenAmount(
-                contractSize,
-                strike64x64,
-                isCall
-            );
+        // calculate unconsumed APY fee so that it may be refunded
 
-            // calculate unconsumed APY fee so that it may be refunded
-
-            apyFee = _calculateApyFee(l, shortTokenId, tokenAmount, maturity);
-        }
+        uint256 apyFee = _calculateApyFee(
+            l,
+            shortTokenId,
+            tokenAmount,
+            maturity
+        );
 
         EnumerableSet.AddressSet storage underwriters = ERC1155EnumerableStorage
             .layout()
