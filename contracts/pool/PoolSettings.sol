283da49e60a236c760d9341ce20f748433c8ff74
Nick Barry <itsnickbarry@protonmail.ch>
2022-05-22 19:33:50 +0300

    add function to process stuck reserved APY fees

--- contracts/pool/PoolSettings.sol
+++ contracts/pool/PoolSettings.sol
@@ -3,10 +3,11 @@
 
 pragma solidity ^0.8.0;
 
-import {PoolStorage} from "./PoolStorage.sol";
+import {ABDKMath64x64} from "abdk-libraries-solidity/ABDKMath64x64.sol";
 
 import {IPoolSettings} from "./IPoolSettings.sol";
 import {PoolInternal} from "./PoolInternal.sol";
+import {PoolStorage} from "./PoolStorage.sol";
 
 /**
  * @title Premia option pool
@@ -14,6 +15,15 @@ import {PoolInternal} from "./PoolInternal.sol";
  */
 contract PoolSettings is IPoolSettings, PoolInternal {
     using PoolStorage for PoolStorage.Layout;
+    using ABDKMath64x64 for int128;
+
+    struct APYFeeData {
+        address underwriter;
+        uint256 shortTokenId;
+        bool isCallPool;
+        int128 feeDiscount64x64;
+        bool divest;
+    }
 
     constructor(
         address ivolOracle,
@@ -34,6 +44,66 @@ contract PoolSettings is IPoolSettings, PoolInternal {
             feeApy64x64
         )
     {}
+
+    function processApyFees(APYFeeData[] calldata apyFeeData)
+        external
+        onlyProtocolOwner
+    {
+        unchecked {
+            PoolStorage.Layout storage l = PoolStorage.layout();
+
+            uint256 callFeesPaid;
+            uint256 putFeesPaid;
+
+            for (uint256 i; i < apyFeeData.length; i++) {
+                APYFeeData memory data = apyFeeData[i];
+
+                address underwriter = data.underwriter;
+                uint256 shortTokenId = data.shortTokenId;
+                bool isCallPool = data.isCallPool;
+
+                uint256 feesReserved = l.feesReserved[underwriter][
+                    shortTokenId
+                ];
+                delete l.feesReserved[underwriter][shortTokenId];
+
+                uint256 rebate = data.feeDiscount64x64.mulu(feesReserved);
+                uint256 feesPaid = feesReserved - rebate;
+
+                _processAvailableFunds(
+                    underwriter,
+                    rebate,
+                    isCallPool,
+                    data.divest,
+                    false
+                );
+
+                if (isCallPool) {
+                    callFeesPaid += feesPaid;
+                } else {
+                    putFeesPaid += feesPaid;
+                }
+
+                emit APYFeePaid(underwriter, shortTokenId, feesPaid);
+            }
+
+            _processAvailableFunds(
+                FEE_RECEIVER_ADDRESS,
+                callFeesPaid,
+                true,
+                true,
+                false
+            );
+
+            _processAvailableFunds(
+                FEE_RECEIVER_ADDRESS,
+                putFeesPaid,
+                false,
+                true,
+                false
+            );
+        }
+    }
 
     /**
      * @inheritdoc IPoolSettings
