c0e619cf5c05e65114bbd5c078cc2e409fa275f7
Lumyo <lumyo.b@gmail.com>
2022-05-25 01:15:24 +0200

    Cleanup

--- contracts/layerZero/interfaces/ILayerZeroReceiver.sol
+++ contracts/layerZero/interfaces/ILayerZeroReceiver.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.0;
 interface ILayerZeroReceiver {
     /*
      * @notice LayerZero endpoint will invoke this function to deliver the message on the destination
-     * @param _srcChainId - the source endpoint identifier
-     * @param _srcAddress - the source sending contract address from the source chain
-     * @param _nonce - the ordered message nonce
-     * @param _payload - the signed payload is the UA bytes has encoded to be sent
+     * @param srcChainId - the source endpoint identifier
+     * @param srcAddress - the source sending contract address from the source chain
+     * @param nonce - the ordered message nonce
+     * @param payload - the signed payload is the UA bytes has encoded to be sent
      */
     function lzReceive(
         uint16 srcChainId,
