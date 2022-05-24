e4b4dccc3bf48022a69135b96b53c4f0ce5fa8d5
Lumyo <lumyo.b@gmail.com>
2022-05-24 19:37:22 +0200

    Layer Zero

---
+++ contracts/layerZero/lzApp/LzAppStorage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BUSL-1.1
+// For further clarification please see https://license.premia.legal
+
+pragma solidity ^0.8.0;
+
+library LzAppStorage {
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("premia.contracts.storage.LzApp");
+
+    struct Layout {
+        mapping(uint16 => bytes) trustedRemoteLookup;
+    }
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
---
+++ contracts/layerZero/lzApp/NonblockingLzAppStorage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BUSL-1.1
+// For further clarification please see https://license.premia.legal
+
+pragma solidity ^0.8.0;
+
+library NonblockingLzAppStorage {
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("premia.contracts.storage.NonblockingLzApp");
+
+    struct Layout {
+        mapping(uint16 => mapping(bytes => mapping(uint64 => bytes32))) failedMessages;
+    }
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
