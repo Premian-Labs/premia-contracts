62783883287917d3e8fc06420befe73c11e90fb9
Lumyo <lumyo.b@gmail.com>
2022-05-27 00:48:26 +0200

    VePremiaProxy.sol

---
+++ contracts/staking/VePremiaProxy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+// For further clarification please see https://license.premia.legal
+
+pragma solidity ^0.8.0;
+
+import {ProxyUpgradeableOwnable} from "../ProxyUpgradeableOwnable.sol";
+import {ERC20MetadataStorage} from "@solidstate/contracts/token/ERC20/metadata/ERC20MetadataStorage.sol";
+
+import {PremiaStakingStorage} from "./PremiaStakingStorage.sol";
+
+contract VePremiaProxy is ProxyUpgradeableOwnable {
+    using ERC20MetadataStorage for ERC20MetadataStorage.Layout;
+
+    constructor(address implementation)
+        ProxyUpgradeableOwnable(implementation)
+    {
+        ERC20MetadataStorage.Layout storage l = ERC20MetadataStorage.layout();
+        l.setName("Voting Escrow Premia");
+        l.setSymbol("vePREMIA");
+        l.setDecimals(18);
+
+        PremiaStakingStorage.layout().withdrawalDelay = 10 days;
+    }
+}
