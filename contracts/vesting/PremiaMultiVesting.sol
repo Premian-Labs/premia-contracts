eebf7ab059e60ece334b572ebc132a2e0d0a5587
Nick Barry <itsnickbarry@protonmail.ch>
2022-06-02 02:54:44 +0300

    rearrange imports

--- contracts/vesting/PremiaMultiVesting.sol
+++ contracts/vesting/PremiaMultiVesting.sol
@@ -3,11 +3,10 @@
 
 pragma solidity ^0.8.0;
 
-import {SafeERC20} from "@solidstate/contracts/utils/SafeERC20.sol";
-import {IERC20} from "@solidstate/contracts/token/ERC20/IERC20.sol";
-
 import {Ownable} from "@solidstate/contracts/access/Ownable.sol";
 import {OwnableStorage} from "@solidstate/contracts/access/OwnableStorage.sol";
+import {IERC20} from "@solidstate/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@solidstate/contracts/utils/SafeERC20.sol";
 
 /**
  * @author Premia
