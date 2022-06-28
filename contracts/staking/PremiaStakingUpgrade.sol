151eab5eb90d45fab2d70820a79a06d9c8df21bd
Lumyo <lumyo.b@gmail.com>
2022-06-28 17:59:39 +0200

    Add UserUpgraded event

--- contracts/staking/PremiaStakingUpgrade.sol
+++ contracts/staking/PremiaStakingUpgrade.sol
@@ -13,6 +13,14 @@ import {PremiaStakingStorage} from "./PremiaStakingStorage.sol";
 contract PremiaStakingUpgrade is ERC20, OwnableInternal {
     using SafeCast for uint256;
 
+    event UserUpgraded(
+        address indexed user,
+        uint256 oldStake,
+        uint256 stakePeriod,
+        uint256 oldBalance,
+        uint256 newBalance
+    );
+
     address internal immutable PREMIA;
     uint256 internal constant INVERSE_BASIS_POINT = 1e4;
 
@@ -57,6 +65,8 @@ contract PremiaStakingUpgrade is ERC20, OwnableInternal {
         uint256 newBalance = ((oldBalance + oldStake) *
             oldL.xPremiaToPremiaRatio) / 1e18;
 
+        uint256 stakePeriod = user.stakePeriod;
+
         delete oldL.userInfo[userAddress];
 
         l.totalPower += _calculateStakeAmountWithBonus(newBalance, 0);
@@ -64,7 +74,13 @@ contract PremiaStakingUpgrade is ERC20, OwnableInternal {
         _burn(address(this), oldStake);
         _mint(userAddress, newBalance - oldBalance);
 
-        // ToDo : Event ?
+        emit UserUpgraded(
+            userAddress,
+            oldStake,
+            stakePeriod,
+            oldBalance,
+            newBalance
+        );
     }
 
     function _getStakePeriodMultiplier(uint256 period)
