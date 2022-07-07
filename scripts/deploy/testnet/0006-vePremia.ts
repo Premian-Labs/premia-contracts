cdba1a049cec28f66f5466e39424793df20cd4fc
Lumyo <lumyo.b@gmail.com>
2022-07-07 22:20:27 +0200

    Update testnet deployment script

--- scripts/deploy/testnet/0006-vePremia.ts
+++ scripts/deploy/testnet/0006-vePremia.ts
@@ -1,11 +1,11 @@
 import { ethers } from 'hardhat';
 import {
   ERC20Mock__factory,
+  ExchangeHelper__factory,
   VePremia__factory,
   VePremiaProxy__factory,
 } from '../../../typechain';
 import LZ_ENDPOINTS from '../../../constants/layerzeroEndpoints.json';
-import { ZERO_ADDRESS } from '../../../test/utils/constants';
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -21,17 +21,25 @@ async function main() {
     throw new Error('ChainId not implemented');
   }
 
-  console.log('aa');
   const premia = await new ERC20Mock__factory(deployer).deploy('PREMIA', 18);
 
   console.log('Premia : ', premia.address);
   await premia.deployed();
 
+  const usdc = await new ERC20Mock__factory(deployer).deploy('USDC', 6);
+
+  console.log('USDC : ', usdc.address);
+  await usdc.deployed();
+
+  const exchangeHelper = await new ExchangeHelper__factory(deployer).deploy();
+
+  console.log('exchange helper : ', exchangeHelper.address);
+
   const vePremiaImpl = await new VePremia__factory(deployer).deploy(
     lzEndpoint,
     premia.address,
-    ZERO_ADDRESS,
-    ZERO_ADDRESS,
+    usdc.address,
+    exchangeHelper.address,
   );
 
   console.log('vePremia impl : ', vePremiaImpl.address);
