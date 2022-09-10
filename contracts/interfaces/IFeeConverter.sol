e7062449c4ff70c7fa62802d022e31ee900e34c9
Lumyo <lumyo.b@gmail.com>
2022-09-11 01:04:05 +0200

    Update PremiaMaker and rename to FeeConverter

---
+++ contracts/interfaces/IFeeConverter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+interface IFeeConverter {
+    event Converted(
+        address indexed account,
+        address indexed token,
+        uint256 inAmount,
+        uint256 outAmount
+    );
+
+    event SetAuthorized(address indexed account, bool isAuthorized);
+
+    function convert(
+        address sourceToken,
+        address callee,
+        address allowanceTarget,
+        bytes calldata data
+    ) external;
+}
