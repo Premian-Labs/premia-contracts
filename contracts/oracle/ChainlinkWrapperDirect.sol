6566638f7032acbb20677f252193a383991171ed
Lumyo <lumyo@proton.me>
2023-03-29 17:01:41 +0200

    Fixes from review

--- contracts/oracle/ChainlinkWrapperDirect.sol
+++ contracts/oracle/ChainlinkWrapperDirect.sol
@@ -1,23 +1,38 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: BUSL-1.1
+// For further clarification please see https://license.premia.legal
 
 pragma solidity ^0.8.0;
 
 import {AggregatorInterface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorInterface.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+import {IChainlinkWrapperInternal} from "./IChainlinkWrapperInternal.sol";
+
 /// @notice Used as a replacement of ChainlinkWrapper, to use a Chainlink price feed deployed afterwards
-contract ChainlinkWrapperDirect {
-    address internal immutable CHAINLINK_PRICE_FEED;
+contract ChainlinkWrapperDirect is IChainlinkWrapperInternal {
+    AggregatorV3Interface internal immutable CHAINLINK_PRICE_FEED;
 
     constructor(address chainlinkPriceFeed) {
-        CHAINLINK_PRICE_FEED = chainlinkPriceFeed;
+        CHAINLINK_PRICE_FEED = AggregatorV3Interface(chainlinkPriceFeed);
     }
 
     function latestAnswer() external view returns (int256) {
-        return AggregatorInterface(CHAINLINK_PRICE_FEED).latestAnswer();
+        try CHAINLINK_PRICE_FEED.latestRoundData() returns (
+            uint80,
+            int256 answer,
+            uint256,
+            uint256,
+            uint80
+        ) {
+            return answer;
+        } catch Error(string memory reason) {
+            revert(reason);
+        } catch (bytes memory data) {
+            revert ChainlinkWrapper__LatestRoundDataCallReverted(data);
+        }
     }
 
     function decimals() external view returns (uint8) {
-        return AggregatorV3Interface(CHAINLINK_PRICE_FEED).decimals();
+        return CHAINLINK_PRICE_FEED.decimals();
     }
 }
